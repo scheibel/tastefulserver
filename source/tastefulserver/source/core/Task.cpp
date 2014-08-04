2ba7419d4d4b0608d6f6b6868a85d6ca483f10d0
Willy Scheibel <willyscheibel@gmx.de>
2014-08-04 15:59:44 +0200

    Use MIT license (refs #10)

--- source/tastefulserver/source/core/Task.cpp
+++ source/tastefulserver/source/core/Task.cpp
@@ -1,29 +1,3 @@
-/**
- * (C) LGPL-3
- *
- * Tasteful Server <https://github.com/scheibel/tasteful-server>
- *
- * Copyright: 2012-2014 Lux, Scheibel
- * Authors:
- *     Roland Lux <rollux2000@googlemail.com>
- *     Willy Scheibel <willyscheibel@gmx.de>
- *
- * This file is part of Tasteful Server.
- *
- * Tasteful Server is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Tasteful Server is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Tasteful Server.  If not, see <http://www.gnu.org/licenses/>.
- **/
-
 #include <tastefulserver/Task.h>
 
 namespace tastefulserver {
