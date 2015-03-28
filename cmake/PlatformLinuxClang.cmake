2b3cc144b091b50169758cf2b4280a35cf6d5ff4
Willy Scheibel <willyscheibel@gmx.de>
2015-03-28 22:10:29 +0100

    Update cmake-init

--- cmake/PlatformLinuxClang.cmake
+++ cmake/PlatformLinuxClang.cmake
@@ -3,17 +3,9 @@ message(STATUS "Configuring for platform Linux/GCC.")
 
 # Enable C++11 support
 
-execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion
-	OUTPUT_VARIABLE GCC_VERSION)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
 
-if(GCC_VERSION VERSION_GREATER 4.7 OR GCC_VERSION VERSION_EQUAL 4.7)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++11")
-elseif(GCC_VERSION VERSION_EQUAL 4.6)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++0x")
-else()
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
-endif()
-
+include_directories("/usr/include/c++/v1/")
 
 set(LINUX_COMPILE_DEFS
 	LINUX	                  # Linux system
@@ -29,7 +21,7 @@ set(DEFAULT_COMPILE_DEFS_RELEASE
     NDEBUG	                  # Release build
 )
 
-set(LINUX_COMPILE_FLAGS "-fvisibility=hidden -pthread -pipe -fPIC -Werror -Wall -pedantic -Wextra -Wfloat-equal -Wcast-qual -Wcast-align -Wno-error=float-equal -Wno-error=switch -Wreturn-type -fno-exceptions -Wtrampolines")
+set(LINUX_COMPILE_FLAGS "-fvisibility=hidden -pthread -pipe -fPIC -Werror -Wall -pedantic -Wextra -Wfloat-equal -Wcast-qual -Wcast-align -Wno-error=float-equal -Wno-error=switch -Wreturn-type -fno-exceptions -Wno-mismatched-tags -Wno-unsequenced -Wno-sign-conversion -Wno-unused-function -Wno-missing-braces")
 # pthread       -> use pthread library
 # no-rtti       -> disable c++ rtti
 # no-exceptions -> disable exception handling
@@ -37,8 +29,7 @@ set(LINUX_COMPILE_FLAGS "-fvisibility=hidden -pthread -pipe -fPIC -Werror -Wall 
 # fPIC          -> use position independent code
 # -Wreturn-type -Werror=return-type -> missing returns in functions and methods are handled as errors which stops the compilation
 # -Wshadow -> e.g. when a parameter is named like a member, too many warnings, disabled for now
-# -fvisibility=hidden -> only export symbols with __attribute__ ((visibility ("default")))
-
+# -fvisibility=hidden -> only export symbols with __attribute__
 set(LINUX_LINKER_FLAGS "-pthread")
 
 set(DEFAULT_COMPILE_FLAGS ${LINUX_COMPILE_FLAGS})
